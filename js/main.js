6fbb720beb8f00278d506f2a84e18ddbaf442b74
Vinícius R. Cerqueira <vinicius.cerqueir.pb@compasso.com.br>
2022-02-14 04:24:56 -0300

    merging branches

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
 var register = document.querySelector(".register");
-var fullname = document.getElementById('fullname');
-var name = document.querySelector(".fullnameInput");
 
 register.addEventListener("click", validate)
 
-function validate(){
-    if(document.querySelector(".checkboxInput").checked){
-        alert("checked");
+/*function validate(){
+    var form = document.getElementById('form');
+    if(document.getElementById("checkboxin").checked){
+        alert(form.fullnamein.value);
     }else{
         alert("not checked bruh");
     }
-};
+};*/
 
+function validate(){
+    var form = document.getElementById('form');
+    if(namecheck() = true){
+        alert(form.fullnamein.value);
+    }else{
+        alert("not checked bruh");
+}
 
-var form = document.getElementById('form');
-var teste = document.querySelector('.fullnameInput');
+function namecheck(){
+    var form = document.getElementById('form');
+    if(form.fullnamein.value > 4){
+        alert(form.fullnamein.value);
+    }
 
-form.addEventListener('submit', function(e) {
-    // alerta o valor do campo
-    alert(teste.value);
+    else{
 
-    // impede o envio do form
-    e.preventDefault();
-});
-
-//function namecheck("name"){}
+    }
+}
 
 // FUNCTIONS
